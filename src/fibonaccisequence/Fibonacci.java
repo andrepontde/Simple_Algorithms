da4a85c13fe21ad4c5236904461017f3f7125568
Andre Pont De Anda <x23164034@student.ncirl.ie>
2025-02-14 09:33:43 +0000

    First (and probably last) commit

---
+++ src/fibonaccisequence/Fibonacci.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fibonaccisequence;
+
+/**
+ *
+ * @author AndrePontDeAnda
+ */
+public class Fibonacci {
+
+    public Fibonacci() {
+    }
+
+    private static int fibonacci(int n) {
+        int fib;
+        if ((n == 1) || (n == 2)) {
+            fib = 1;
+        } else {
+            fib = fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        return fib;
+    }
+    
+    public static void series(int n){
+        int i;
+        for (i = 1; i < n; i++){
+            System.out.println("Element"+i+ "is "+ fibonacci(i));
+        }
+    }
+    
+    public static void main(String[] args){
+        series(12);
+    }
+}
